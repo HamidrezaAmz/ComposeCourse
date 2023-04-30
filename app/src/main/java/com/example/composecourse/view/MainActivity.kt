620e36de89838e578bbe13b8c7bc4930554da29f
amuzadeh <hamidreza7150@gmail.com>
2023-04-30 17:52:04 +0330

    second commit

---
+++ app/src/main/java/com/example/composecourse/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.composecourse.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.composecourse.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
