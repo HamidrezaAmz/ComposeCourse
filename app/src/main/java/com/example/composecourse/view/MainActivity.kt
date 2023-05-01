c95f5c5df349d38aeef7d7abb6c65be4b51a5c3c
amuzadeh <hamidreza7150@gmail.com>
2023-05-01 15:00:03 +0330

    course 1 added and developed

--- app/src/main/java/com/example/composecourse/view/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/view/MainActivity.kt
@@ -1,14 +1,42 @@
 package com.example.composecourse.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.example.composecourse.R
+import com.example.composecourse.view.course_1.Course1Activity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initListeners()
+
+        // todo: just for dev, will be removed after developing
+        navigateToTargetActivity(Course1Activity())
+    }
+
+    private fun initListeners() {
+        findViewById<AppCompatButton>(R.id.appCompatButton_course_1).setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.appCompatButton_course_1 -> {
+                navigateToTargetActivity(Course1Activity())
+            }
+        }
+    }
+
+    private fun navigateToTargetActivity(targetComponentActivity: ComponentActivity) {
+        val targetIntent = Intent(this@MainActivity, targetComponentActivity::class.java)
+        startActivity(targetIntent)
     }
 
 }
---
+++ app/src/main/java/com/example/composecourse/view/course_1/Course1Activity.kt
@@ -0,0 +1,40 @@
+package com.example.composecourse.view.course_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.composecourse.view.course_1.ui.theme.ComposeCourseTheme
+
+class Course1Activity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(modifier = Modifier.background(Color.Green)) {
+                    Text(text = "first column - row 1")
+                }
+                Row(modifier = Modifier.background(Color.Cyan)) {
+                    Text(text = "second column - row 2")
+                }
+            }
+        }
+    }
+}
